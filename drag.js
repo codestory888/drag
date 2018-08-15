67c903cf67749dc71497f602ba8e2d9146bf35e4
codestory888 <codestory@qq.com>
2018-08-15 21:08:45 +0800

    success drag

--- drag.js
+++ drag.js
@@ -1,6 +1,16 @@
 function drag(id){
  	var Obj=document.getElementById(id);
- 	obj.onmousedown=function(ev){
- 		disX=ev.pageX;
+ 	Obj.onmousedown=function(ev){
+ 		disX=ev.pageX-Obj.offsetLeft;
+ 		disY=ev.pageY-Obj.offsetTop;
+ 		document.onmousemove=function(ev){
+ 			Obj.style.left =ev.pageX-disX+'px';
+ 			Obj.style.top =ev.pageY-disY+'px';
+ 		}
+ 		document.onmouseup=function(){
+ 			document.onmousemove=null;
+ 			document.onmouseup=null;
+ 		}
  	}
+ 	return false;
 }
